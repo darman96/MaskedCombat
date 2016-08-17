9357bbb609f849c0a54bfb4528c0e7e078c50fd6
Erik Simon <simonerik1996@gmail.com>
2016-08-17 20:38:48 +0200

    Mask Cycle UI and Placeholder Icons

--- Assets/TheProject/Scripts/UI/GUIController.cs
+++ Assets/TheProject/Scripts/UI/GUIController.cs
@@ -12,6 +12,12 @@ using System.Collections.Generic;
 
 public class GUIController : MonoBehaviour
 {
+
+    public MaskCycle player1Cycle;
+    public MaskCycle player2Cycle;
+    public MaskCycle player3Cycle;
+    public MaskCycle player4Cycle;
+
     private PlayerController pPlayer1;
     private PlayerController pPlayer2;
     private PlayerController pPlayer3;
@@ -27,5 +33,26 @@ public class GUIController : MonoBehaviour
 
     void Update()
     {
+
+        if (player1Cycle.ActiveOffensive != pPlayer1.ActiveMask_Offensive)
+            player1Cycle.ActiveOffensive = pPlayer1.ActiveMask_Offensive;
+        if (player1Cycle.ActiveDefensive != pPlayer1.ActiveMask_Defensive)
+            player1Cycle.ActiveDefensive = pPlayer1.ActiveMask_Defensive;
+
+        if (player2Cycle.ActiveOffensive != pPlayer2.ActiveMask_Offensive)
+            player2Cycle.ActiveOffensive = pPlayer2.ActiveMask_Offensive;
+        if (player2Cycle.ActiveDefensive != pPlayer2.ActiveMask_Defensive)
+            player2Cycle.ActiveDefensive = pPlayer2.ActiveMask_Defensive;
+
+        if (player3Cycle.ActiveOffensive != pPlayer3.ActiveMask_Offensive)
+            player3Cycle.ActiveOffensive = pPlayer3.ActiveMask_Offensive;
+        if (player3Cycle.ActiveDefensive != pPlayer3.ActiveMask_Defensive)
+            player3Cycle.ActiveDefensive = pPlayer3.ActiveMask_Defensive;
+
+        if (player4Cycle.ActiveOffensive != pPlayer4.ActiveMask_Offensive)
+            player4Cycle.ActiveOffensive = pPlayer4.ActiveMask_Offensive;
+        if (player4Cycle.ActiveDefensive != pPlayer4.ActiveMask_Defensive)
+            player4Cycle.ActiveDefensive = pPlayer4.ActiveMask_Defensive;
+
     }
 }
---
+++ Assets/TheProject/Scripts/UI/MaskCycle.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MaskCycle : MonoBehaviour {
+
+    public MaskType ActiveOffensive
+    {
+        get { return activeOffensive; }
+        set
+        {
+            activeOffensive = value;
+            UpdateHighlight();
+        }
+    }
+    public MaskType ActiveDefensive
+    {
+        get { return activeDefensive; }
+        set
+        {
+            activeDefensive = value;
+            UpdateHighlight();
+        }
+    }
+
+    public Image highlightOffensive1;
+    public Image highlightOffensive2;
+    public Image highlightOffensive3;
+    public Image highlightOffensive4;
+
+    public Image highlightDefensive1;
+    public Image highlightDefensive2;
+    public Image highlightDefensive3;
+    public Image highlightDefensive4;
+
+    private MaskType activeOffensive;
+    private MaskType activeDefensive;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void UpdateHighlight()
+    {
+        switch(activeOffensive)
+        {
+            case MaskType.Fire:
+                highlightOffensive1.enabled = true;
+                highlightOffensive2.enabled = false;
+                highlightOffensive3.enabled = false;
+                highlightOffensive4.enabled = false;
+                break;
+            case MaskType.Water:
+                highlightOffensive1.enabled = false;
+                highlightOffensive2.enabled = true;
+                highlightOffensive3.enabled = false;
+                highlightOffensive4.enabled = false;
+                break;
+            case MaskType.Earth:
+                highlightOffensive1.enabled = false;
+                highlightOffensive2.enabled = false;
+                highlightOffensive3.enabled = true;
+                highlightOffensive4.enabled = false;
+                break;
+            case MaskType.Lightning:
+                highlightOffensive1.enabled = false;
+                highlightOffensive2.enabled = false;
+                highlightOffensive3.enabled = false;
+                highlightOffensive4.enabled = true;
+                break;
+        }
+
+        switch (activeDefensive)
+        {
+            case MaskType.Ice:
+                highlightDefensive1.enabled = true;
+                highlightDefensive2.enabled = false;
+                highlightDefensive3.enabled = false;
+                highlightDefensive4.enabled = false;
+                break;
+            case MaskType.Wind:
+                highlightDefensive1.enabled = false;
+                highlightDefensive2.enabled = true;
+                highlightDefensive3.enabled = false;
+                highlightDefensive4.enabled = false;
+                break;
+            case MaskType.Metal:
+                highlightDefensive1.enabled = false;
+                highlightDefensive2.enabled = false;
+                highlightDefensive3.enabled = true;
+                highlightDefensive4.enabled = false;
+                break;
+            case MaskType.Nature:
+                highlightDefensive1.enabled = false;
+                highlightDefensive2.enabled = false;
+                highlightDefensive3.enabled = false;
+                highlightDefensive4.enabled = true;
+                break;
+        }
+    }
+
+}
